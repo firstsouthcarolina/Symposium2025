8c797e33fddeea6df178975a13fcbaf6685cc49c
Judson James <judsonjames96@gmail.com>
2025-11-07 20:10:08 -0500

    Track 01 Virtually Finished

---
+++ Track02/01_Simulation/simplesimulation/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.pivot.Pivot;
+import frc.robot.subsystems.pivot.PivotSim;
+import frc.robot.subsystems.roller.Roller;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+@Logged
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Roller roller = new Roller();
+  private final Pivot pivot = new Pivot();
+
+  private final PivotSim pivotSim;
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+    if (RobotBase.isSimulation()) {
+      pivotSim = new PivotSim(pivot);
+    } else {
+      pivotSim = null;
+    }
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
+    // cancelling on release.
+    m_driverController.leftTrigger().whileTrue(roller.runVoltsCommand(6.0));
+    m_driverController.rightTrigger().whileTrue(roller.runVoltsCommand(-6.0));
+
+    m_driverController.rightBumper().onTrue(pivot.setAngleCommand(45, 10));
+    m_driverController.leftBumper().onTrue(pivot.setAngleCommand(-45, 10));
+
+    m_driverController.povUp().onTrue(pivot.moveToAngleCommand(0));
+    m_driverController.povLeft().onTrue(pivot.moveToAngleCommand(90));
+    m_driverController.povRight().onTrue(pivot.moveToAngleCommand(-90));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Commands.none();
+  }
+}
---
+++ Track02/01_Simulation/simplesimulation/src/main/java/frc/robot/util/ControlMode.java
@@ -0,0 +1,10 @@
+package frc.robot.util;
+
+/**
+ * Handles Control Modes for Motors and Systems
+ */
+public enum ControlMode {
+    OPEN_LOOP,
+    POSITION,
+    VELOCITY,
+}
